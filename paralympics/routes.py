fc3f1c103f538a121d14bde55d3bbf069fae98aa
irfan <irfan.roslan.21@ucl.ac.uk>
2024-01-23 23:08:48 +0000

    Finish coding activities for week 3

--- paralympics/routes.py
+++ paralympics/routes.py
@@ -1,4 +1,6 @@
 from flask import current_app as app
+from flask import request
+from flask import make_response
 from paralympics.schemas import RegionSchema, EventSchema
 from paralympics import db
 from paralympics.models import Region, Event
@@ -52,3 +54,105 @@ def get_region(NOC):
     result = region_schema.dump(region)
     # Return the data
     return result
+
+@app.post('/events')
+def add_event():
+    """ Adds a new event.
+
+    Gets the JSON data from the request body and uses this to deserialise JSON to an object using Marshmallow 
+    event_schema.load()
+
+    :returns: JSON"""
+    ev_json = request.get_json()
+    event = event_schema.load(ev_json)
+    db.session.add(event)
+    db.session.commit()
+    return {"message": f"Event added with id= {event.id}"}
+
+@app.post('/regions')
+def add_region():
+    """ Adds a new region.
+
+    Gets the JSON data from the request body and uses this to deserialise JSON to an object using Marshmallow 
+    region_schema.load()
+
+    :returns: JSON"""
+    reg_json = request.get_json()
+    region = region_schema.load(reg_json)
+    db.session.add(region)
+    db.session.commit()
+    return {"message": f"Region added with NOC= {region.NOC}"}
+
+@app.delete('/events/<int:event_id>')
+def delete_event(event_id):
+    """ Deletes an event.
+
+    Gets the event from the database and deletes it.
+
+    :returns: JSON"""
+    event = db.session.execute(db.select(Event).filter(Event.id == event_id)).scalar_one_or_none()
+    db.session.delete(event)
+    db.session.commit()
+    return {"message": f"Event deleted with id= {event_id}"}
+
+@app.delete('/regions/<string:NOC>')
+def delete_region(NOC):
+    """ Deletes a region.
+
+    Gets the region from the database and deletes it.
+
+    :returns: JSON"""
+    region = db.session.execute(db.select(Region).filter(Region.NOC == NOC)).scalar_one_or_none()
+    db.session.delete(region)
+    db.session.commit()
+    return {"message": f"Region deleted with NOC= {NOC}"}
+
+@app.patch('/events/<int:event_id>')
+def event_update(event_id):
+    """Updates changed fields for the event.
+
+    """
+    # Find the event in the database
+    existing_event = db.session.execute(
+        db.select(Event).filter(Event.id == event_id)
+    ).scalar_one_or_none()
+    # Get the updated details from the json sent in the HTTP patch request
+    event_json = request.get_json()
+    # Use Marshmallow to update the existing records with the changes from the json
+    event_update = event_schema.load(event_json, instance=existing_event, partial=True)
+    # Commit the changes to the database
+    db.session.add(event_update)
+    db.session.commit()
+    # Return json showing the updated record
+    updated_event = db.session.execute(
+        db.select(Event).filter(Event.id == event_id)
+    ).scalar_one_or_none()
+    result = event_schema.jsonify(updated_event)
+    response = make_response(result, 200)
+    response.headers["Content-Type"] = "application/json"
+    return response
+
+@app.patch('/regions/<string:NOC>')
+def region_update(NOC):
+    """Updates changed fields for the region.
+
+    """
+    # Find the region in the database
+    existing_region = db.session.execute(
+        db.select(Region).filter(Region.NOC == NOC)
+    ).scalar_one_or_none()
+    # Get the updated details from the json sent in the HTTP patch request
+    region_json = request.get_json()
+    # Use Marshmallow to update the existing records with the changes from the json
+    region_update = region_schema.load(region_json, instance=existing_region, partial=True)
+    # Commit the changes to the database
+    db.session.add(region_update)
+    db.session.commit()
+    # Return json showing the updated record
+    updated_region = db.session.execute(
+        db.select(Region).filter(Region.NOC == NOC)
+    ).scalar_one_or_none()
+    result = region_schema.jsonify(updated_region)
+    response = make_response(result, 200)
+    response.headers["Content-Type"] = "application/json"
+    return response
